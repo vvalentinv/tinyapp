7ac4e1149cefc7752926e375cbb5ad03eea3935e
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-06 21:31:37 -0500

    add urls view OK

--- express_server.js
+++ express_server.js
@@ -13,8 +13,9 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
