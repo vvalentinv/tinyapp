05a00602571e1132f199d818d4b7349934c77a5e
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-07 02:13:33 -0500

    add post, persist to server instance, redirect to site by clicking short url only from existing object

--- express_server.js
+++ express_server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const MAXNOCHARACTERS = 6;
+
+//Implement the function generateRandomString() here
+const rndStr = function generateRandomString() {
+  let tiny = "";
+  const pool = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  for (let i = 0; i < MAXNOCHARACTERS; i++) {
+    tiny += pool[Math.floor(Math.random() * pool.length)]
+  }
+  return tiny;
+
+}
+//
 
 app.set("view engine", "ejs");
 
@@ -8,6 +21,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,12 +34,30 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const tiny = rndStr();//reference value to use in redirect
+  urlDatabase[tiny] = req.body.longURL;// persist data
+  res.redirect(`/urls/${tiny}`);         // to url details page
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
+
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
 });
 
